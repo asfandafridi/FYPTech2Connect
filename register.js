66aafea14156c205720265c50dc4373584978d42
asfandafridi <asfandafridi97@gmail.com>
2024-04-21 13:14:42 +0500

    first commit

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUTKAMecKFlrGEJY9VrwY9tOoCussvSME",
+  authDomain: "t2c-signup.firebaseapp.com",
+  projectId: "t2c-signup",
+  storageBucket: "t2c-signup.appspot.com",
+  messagingSenderId: "724382035337",
+  appId: "1:724382035337:web:168011cdd34a5047fe0558"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
